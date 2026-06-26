61674e020fd87cc82e3ff005e7c59f2436b57def
Bastian <bacabezas2025@udec.cl>
2026-06-26 01:10:30 -0400

    Modificación en el alto de la ventana en clase 'Ventana'

--- GestionTorneos/src/main/java/GUI/Ventana.java
+++ GestionTorneos/src/main/java/GUI/Ventana.java
@@ -7,7 +7,7 @@ public class Ventana extends JFrame {
         JFrame ventana = new JFrame();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Gestión de torneos");
-        this.setSize(1200, 830);
+        this.setSize(1200, 820);
         this.setVisible(true);
         this.setResizable(false);
         this.add(ventana);
