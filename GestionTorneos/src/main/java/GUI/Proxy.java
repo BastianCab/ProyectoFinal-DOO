c4a51f7f672d57b6c7eae9d415c4d00be1da82b2
Bastian <bacabezas2025@udec.cl>
2026-06-27 04:05:23 -0400

    Añadido JavaDoc e implementación base del Action Listener al Proxy

--- GestionTorneos/src/main/java/GUI/Proxy.java
+++ GestionTorneos/src/main/java/GUI/Proxy.java
@@ -2,24 +2,61 @@ package GUI;
 
 import Logica.*;
 
+/**
+ * Implementación del patrón "Proxy", para conectar la GUI con la lógica del gestor
+ */
 public class Proxy {
     Organizador organizador;
     Torneo torneo;
     Participante participante;
+
+    /**
+     * Inicializa el proxy y sus atributos
+     */
     public Proxy() {
-        organizador = new Organizador("Pepe", 1);
+        organizador = new Organizador("Pepe");
         torneo = new Torneo();
         participante = new Participante("Carlitos");
     }
 
+    /**
+     * El método para que el proxy sepa que hacer una vez se presione algún boton
+     * de un panel cualquiera
+     * @param boton el botón específico el cual se presionó
+     */
+    //Botones del panel principal: Organizar torneo, modificar torneo y espectar
+    public void actionClick(int boton) {
+        switch (boton) {
+            case (1):
+                break;
+            case (2):
+                break;
+            case (3):
+                break;
+        }
+    }
+
+    /**
+     * Getter del organizador
+     * @return el organizador del torneo
+     */
     public Organizador getOrganizador() {
         return organizador;
     }
 
+    /**
+     * Getter del torneo
+     * @return el torneo creado
+     */
     public Torneo getTorneo() {
         return torneo;
     }
 
+
+    /**
+     * Getter del participante
+     * @return el participante
+     */
     public Participante getParticipante() {
         return participante;
     }
