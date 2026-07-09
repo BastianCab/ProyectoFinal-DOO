166d1c45b49f42f305973dba6d19290048df9b09
Bastian <bacabezas2025@udec.cl>
2026-07-08 22:59:38 -0400

    Creación tests unitarios para clases 'Enfrentamiento', 'Participante' y 'Torneo'

---
+++ GestionTorneos/src/test/java/Logica/ParticipanteTest.java
@@ -0,0 +1,32 @@
+package Logica;
+
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
+
+public class ParticipanteTest {
+    @Test
+    @DisplayName("Test todo correcto")
+    void testParticipanteCorrecto() throws DatoInvalidoException {
+        Participante participante = new Participante(TipoParticipante.PERSONA, "Jugador1");
+
+        assertEquals(TipoParticipante.PERSONA, participante.getTipo());
+        assertEquals("Jugador1", participante.getNombre());
+    }
+
+    @Test
+    @DisplayName("Test nombre vacío o nulo")
+    void testParticipanteNombreNuloVacio() {
+        assertThrows(DatoInvalidoException.class, () ->
+                new Participante(TipoParticipante.PERSONA, null));
+
+        assertThrows(DatoInvalidoException.class, () ->
+                new Participante(TipoParticipante.PERSONA, ""));
+    }
+
+    @Test
+    @DisplayName("Test tipo de participante nulo")
+    void testParticipanteTipoNulo() {
+        assertThrows(DatoInvalidoException.class, () ->
+                new Participante(null, "Jugador1"));
+    }
+}
