ee7651299acd3fa0010f5b078bf18dce0397f089
Ricardo Lagos <olivertvliz22@gmail.com>
2026-07-08 20:08:34 -0400

    arreglar merge

--- GestionTorneos/src/test/java/Logica/CalcularSimpleTest.java
+++ GestionTorneos/src/test/java/Logica/CalcularSimpleTest.java
@@ -9,15 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalcularSimpleTest {
     @Test
-    void jscnkjdnc() {
-        //CalcularSimple bab = new CalcularSimple(new Torneo("A", "a", "a", TipoParticipante.PERSONA, TipoTorneoEnum.LIGA_SIMPLE));
-        void intentarCalulo () throws ErroresCalculo {
-            CalcularSimple bab = new CalcularSimple(10);
-            for (int i = 0; i < 20; i++) {
-                bab.siguiente();
-                bab.enfrentar(1);
-            }
-            ArrayList<String> a = bab.getEnfrentamientosPH();
+    void intentarCalulo () throws ErroresCalculo {
+        CalcularSimple bab = new CalcularSimple(10);
+        for (int i = 0; i < 20; i++) {
+            bab.siguiente();
+            bab.enfrentar(1);
         }
+        ArrayList<String> a = bab.getEnfrentamientosPH();
     }
 }
